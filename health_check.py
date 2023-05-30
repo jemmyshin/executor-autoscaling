470d25b8e4c9d281c9fa5c00631b6dfae694c748
jemmyshin <jem.fu@jina.ai>
2023-05-30 15:43:39 +0800

    docs: add readme

--- health_check.py
+++ health_check.py
@@ -1,11 +1,11 @@
-from jina import Client
-import time
-
-host = ''
-client = Client(host=host)
-
-while 1:
-    start_time = time.time()
-    _ = client.post('/ping')
-    print(f"received response from server within: {time.time() - start_time}")
-    time.sleep(10)
+# from jina import Client
+# import time
+#
+# host = ''
+# client = Client(host=host)
+#
+# while 1:
+#     start_time = time.time()
+#     _ = client.post('/ping')
+#     print(f"received response from server within: {time.time() - start_time}")
+#     time.sleep(10)
